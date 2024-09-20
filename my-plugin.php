d81bb6ccd4a9b36f1f2c0def2e4759b5db90177c
paulnyabaro <paulnyabaro1@gmail.com>
2024-09-20 17:37:55 +0300

    Updated plugin

--- my-plugin.php
+++ my-plugin.php
@@ -2,3 +2,7 @@
  * Plugin Name: Paul Test Plugin
  * Description: Handles the basic functions
  */
+
+ <!-- Removes the admin bar from the front end -->
+
+ add_filter('show_admin_bar', '__return_false');
